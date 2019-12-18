c0aa06afd000f8e9dc34a280a1592225dc2f87a6
Miguerubsk <miguerubsk@gmail.com>
2019-12-18 01:37:05 +0100

    Saludo a usuarios nuevos

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ bot.on('ready', () => {
     bot.user.setActivity('a cazadores en el Smash', { type: 'WATCHING' })
 });
 
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Bienvenido al infierno, ${member}`);
+});
+
 
 bot.on('message', message => {
   // Ignore messages that aren't from a guild
